0d8abb7889249ef3c8ba117190a4e1a6d31204ed
gabriel1379 <gabriel1379@gmail.com>
2020-08-16 20:20:11 +0200

    TG-2 Implement Path-based path handling

--- TransferGenerator/FileHandler.py
+++ TransferGenerator/FileHandler.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import TYPE_CHECKING
 from xml.etree import ElementTree as ET
 
@@ -7,16 +8,16 @@ if TYPE_CHECKING:
 
 class FileHandler:
     def __init__(self, input_path: str, output_path: str):
-        self.__input_path = input_path
-        self.__output_path = output_path
+        self.__input_path: Path = Path(input_path)
+        self.__output_path: Path = Path(output_path)
 
     def load_xml_root(self) -> 'Element':
-        with open(f'{self.__input_path}sample.xml') as transfer_source_file:
+        with open(self.__input_path.joinpath('sample.xml')) as transfer_source_file:
             transfer_source_parsed = ET.parse(transfer_source_file)
             root = transfer_source_parsed.getroot()
 
         return root
 
     def write_out_transfer_code(self, transfer_name: str, transfer_code: str) -> None:
-        with open(f'{self.__output_path}{transfer_name}.py', mode='w') as transfer_target_file:
+        with open(self.__output_path.joinpath(f'{transfer_name}.py'), mode='w') as transfer_target_file:
             transfer_target_file.write(transfer_code)
