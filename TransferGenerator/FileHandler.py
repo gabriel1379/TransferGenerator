0b8c22387093473a4180c6799a75181921972811
gabriel1379 <gabriel1379@gmail.com>
2020-08-16 20:34:45 +0200

    TG-2 Add capability for processing multiple .xml files in source directory (and its subdirectories)

--- TransferGenerator/FileHandler.py
+++ TransferGenerator/FileHandler.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import List, TYPE_CHECKING
 from xml.etree import ElementTree as ET
 
 if TYPE_CHECKING:
@@ -11,13 +11,27 @@ class FileHandler:
         self.__input_path: Path = Path(input_path)
         self.__output_path: Path = Path(output_path)
 
-    def load_xml_root(self) -> 'Element':
-        with open(self.__input_path.joinpath('sample.xml')) as transfer_source_file:
-            transfer_source_parsed = ET.parse(transfer_source_file)
-            root = transfer_source_parsed.getroot()
+    def load_xml_roots(self) -> List['Element']:
+        xml_source_file_paths = self.__scan_for_xmls()
+        xml_roots = []
 
-        return root
+        for xml_source_file_path in xml_source_file_paths:
+            xml_roots.append(
+                self.__load_xml_root(xml_source_file_path)
+            )
+
+        return xml_roots
 
     def write_out_transfer_code(self, transfer_name: str, transfer_code: str) -> None:
         with open(self.__output_path.joinpath(f'{transfer_name}.py'), mode='w') as transfer_target_file:
             transfer_target_file.write(transfer_code)
+
+    def __scan_for_xmls(self) -> List[Path]:
+        return sorted(self.__input_path.glob('**/*_transfers.xml'))
+
+    def __load_xml_root(self, input_path: Path) -> 'Element':
+        with open(input_path) as transfer_source_file:
+            transfer_source_parsed = ET.parse(transfer_source_file)
+            root = transfer_source_parsed.getroot()
+
+        return root
